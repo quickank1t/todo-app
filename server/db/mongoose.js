7f28678ed2ff041441a58ee9837099020c3845bc
Ankit chaurasia <ankit.c987@gmail.com>
2018-05-11 03:58:34 +0200

    :atom: Setup ready for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/todoapp');
+// mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://heroku:qwerty12345@ds217970.mlab.com:17970/todo-app-ankit');
+//
 module.exports = {
   mongoose
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todos} = require('./model/todos');
 const {Users} = require('./model/users');
 
 var app = express();
-
+var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos',(req, res) => {
@@ -20,8 +21,29 @@ app.post('/todos',(req, res) => {
     res.status(400).send(e);
   });
 });
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+
+app.get('/todos',(req, res) =>{
+  Todos.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  })
+});
+
+app.get('/todos/:id',(req, res) => {
+  var id= req.params.id;
+  if(!ObjectID.isValid(id))
+    return res.status(404).send();
+
+  Todos.findById(id).then((todo)=>{
+    if(!todo)
+      return res.status(404).send();
+  res.send({todo});
+  }).catch((e)=> res.status(400).send());
+});
+
+app.listen(port, () => {
+  console.log('Started... ');
 });
 
 module.exports = {app};
